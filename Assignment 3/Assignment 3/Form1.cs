a17c8b32584effc60e09052a3e41028fee66284d
mike alcock <mikejra@hotmail.com>
2015-12-10 13:17:54 -0600

    add from screen started

--- Assignment 3/Assignment 3/Form1.cs
+++ Assignment 3/Assignment 3/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace Assignment_3
 {
@@ -16,6 +18,49 @@ namespace Assignment_3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+        private void addFromWindow()
+        {
+            addNewXML(textBox1.text,textBox2.text,textBox3.text,textBox4.text,);
+        }
+
+        private void addNewXML(String title, int year, int length, String certification,
+           String director, int rating, ArrayList genre, ArrayList actor)
+        {
+            XDocument doc = XDocument.Load("movies.xml");
+            XElement movie = new XElement("movie",
+                new XAttribute("title", title),
+                new XElement("year", year),
+                new XElement("length", length),
+                new XElement("certification", certification),
+                new XElement("director", director),
+                new XElement("rating", rating));
+
+                for (int i = 0; i < genre.Count; i++)
+                {
+                new XElement("genre", genre.);
+                } 
+
+                for (int i = 0; i < actor.Count; i++)
+                {
+                new XElement("genre", actor.elementAt(i));
+                }
+
+            doc.Root.Add(movie);
+            doc.Save("test.xml");
+        }
+
 
     }
 }
